f662f25fe8c71ae7c2b32efd7b614d4c87230691
Raish10100 <raishgma101001@gmail.com>
2025-10-02 09:34:40 +0530

    Signin route and controller implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,7 +15,6 @@ const signupBody = zod.object({
 
 router.post('/signup', async(req, res) => {
     const { success } = signupBody.safeParse(req.body)
-    console.log(req.body)
     if(!success) {
         return res.status(411).json({
             message: "Incorrect inputs"
@@ -49,5 +48,41 @@ router.post('/signup', async(req, res) => {
         token: token 
     })
 })
+
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+router.post("/signin", async(req, res) => {
+    const {success} = signinBody.safeParse(req.body)
+    if(!success) {
+        return res.status(411).json({
+            message: "Incorrect Inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password 
+    })
+
+    if(user) {
+        const token = jwt.sign({
+            userId: user._id 
+        }, JWT_SECRET);
+
+        res.json({
+            token: token 
+        })
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
+
 
 module.exports = router;
